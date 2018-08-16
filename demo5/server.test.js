32c67da7cc1771aebdd82ee86360dc35a94ae210
Level.Z <1171567018@qq.com>
2018-08-16 16:23:01 +0800

    update

--- demo5/server.test.js
+++ demo5/server.test.js
@@ -2,6 +2,8 @@ const app = require('./server');
 const request = require('supertest');
 
 describe('登陆首页', () => {
+  // 设置超时时间
+  this.timeout(10000);
   // 一般建议这步放在钩子中完成
   // 特别注意：agent中的值应该是app.listen的返回值，而不是app
   const agent = request.agent(app.listen(3452));
